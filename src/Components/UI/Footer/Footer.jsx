9a6e07379cf9841e5237285c3f9e9398f4e5beff
PabloGod1326 <juanks339@gmail.com>
2021-11-23 17:26:49 -0500

    Info agregada al Footer

--- src/Components/UI/Footer/Footer.jsx
+++ src/Components/UI/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import './Footer.css'
 
+import facebook from '../../../images/Facebook.png'
+import linkedin from '../../../images/Linkedin.png'
+import twitter from '../../../images/Twitter.png'
+
 export const Footer = () => {
     return (
         <>
             <footer className="divFooter">
-                <h3>Juan Pablo Patiño &copy; 2021.Todos los derechos reservados.</h3>
+                <h3>Juan Pablo Patiño &copy; 2021. Todos los derechos reservados.</h3>
+
+                <div className="redes">
+                    <a href="https://www.facebook.com" target="_blank" ><img src={facebook} alt="Facebook"/></a>
+                    <a href="https://co.linkedin.com" target="_blank" ><img src={linkedin} alt="LinkedIn"/></a>
+                    <a href="https://twitter.com/?lang=es" target="_blank" ><img src={twitter} alt="Twitter"/></a>
+                </div>
+
             </footer>
         </>
     )
--- src/Components/UI/Main1/Main1.jsx
+++ src/Components/UI/Main1/Main1.jsx
@@ -7,10 +7,10 @@ export const Main = () => {
                 <h1>Pagina en React.js</h1>
                 <h3>Algunas Caracteristicas de React</h3>
                 <ul className="classLista">
-                    <li>Es una libreria de Javascript</li>
-                    <li>Funciona en un elemento llamado Virtual DOM</li>
-                    <li>Renderiza Solamente los componentes que necesita</li>
-                    <li>La utilizacion de los Hooks facilita su logica Front-End</li>
+                    <li>Es una libreria de Javascript.</li>
+                    <li>Funciona en un elemento llamado Virtual DOM.</li>
+                    <li>Renderiza solamente los componentes que necesita.</li>
+                    <li>La utilizacion de los Hooks facilita su logica Front-End.</li>
                 </ul>
             </main>
         </>
